66cf3930a63a268853f95171e2e24adb96277f85
valvoda <pepa.valvoda@gmail.com>
2022-09-06 14:32:21 +0100

    lbert influences

--- src/analyse/baselines.py
+++ src/analyse/baselines.py
@@ -363,16 +363,17 @@ if __name__ == '__main__':
 
     tokenized_dir = "../datasets/" + 'precedent' + "/" + 'bert'
 
-    result_path = './outdir/bert/facts/influence_results_tmp_0_False_last-i_294.json'
+    results_path = './outdir/legal_bert/facts/f1f984acd35b4283947585cac74ed6bd_0_250_0_False_last-i_203.json'
+    # result_path = './outdir/bert/facts/influence_results_tmp_0_False_last-i_294.json'
     # result_path = './outdir/bert/facts/927927e50ca941ceb7a0b09b51fe54fb_0_250_0_False_last-i_249.json'
     # result_path = './outdir/bert/both/987cd7bdc92b42afab32772c509aa246_0_10000_0_False_last-i_291.json'
 
 
-    baseline_linear(tokenized_dir, result_path)
+    # baseline_linear(tokenized_dir, result_path)
 
 
-    # baseline_applied(tokenized_dir, result_path)
-    # baseline_avg(tokenized_dir, result_path)
-    # baseline_outcome(tokenized_dir, result_path)
-    # baseline_outcome(tokenized_dir, result_path, True)
-    # baseline_art(tokenized_dir, result_path)
+    baseline_applied(tokenized_dir, result_path)
+    baseline_avg(tokenized_dir, result_path)
+    baseline_outcome(tokenized_dir, result_path)
+    baseline_outcome(tokenized_dir, result_path, True)
+    baseline_art(tokenized_dir, result_path)
