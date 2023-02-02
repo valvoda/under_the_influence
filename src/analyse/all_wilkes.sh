668c942ff959db92fdea12933e503353a656aa2f
valvoda <pepa.valvoda@gmail.com>
2023-02-02 19:21:50 +0900

    wilkes analyse scripts

--- src/analyse/all_wilkes.sh
+++ src/analyse/all_wilkes.sh
@@ -6,7 +6,7 @@ do
     do
       end=$((start+50))
       echo "${start} ${end} ${arch} ${model}"
-      START=$start END=$end MOD=$model ARCH=$arch sbatch run.wilkes
+      START=$start END=$end MOD=$model ARCH=$arch sbatch run.wilkes3
     done
   done
 done
